490c8c3abae3d58860ef3738b373d1a85c8dfa30
open-meadow <rohan.varughese@protonmail.com>
2022-11-14 21:35:31 +0000

    Add assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) console.log(` 💯 Assertion Passed: ${actual} === ${expected}`);
+  else console.log(` 💩 Assertion Failed: ${actual} != ${expected}`);
+};
+
+// Test code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("poopoo","poopoo");
+assertEqual("poopoo","POOPOO");
+assertEqual(123, 234);
