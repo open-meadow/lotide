b808f548ceacca3af0c9965e7eb2803976d582cb
open-meadow <rohan.varughese@protonmail.com>
2022-11-14 22:06:45 +0000

    Add head.js

---
+++ head.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) console.log(` 💯 Assertion Passed: ${actual} === ${expected}`);
+  else console.log(` 💩 Assertion Failed: ${actual} != ${expected}`);
+};
+
+function head (arr) {
+  return arr[0];
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["poopoo","peepee"]), 5);
