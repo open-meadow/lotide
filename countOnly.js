94d6dab7b8d7b5c156dc44fb2eae28ad9c0bb464
open-meadow <rohan.varughese@protonmail.com>
2022-11-16 23:15:27 +0000

    Finish and commit findKeyByValue.js

--- countOnly.js
+++ countOnly.js
@@ -7,7 +7,7 @@ const assertEqual = function(actual, expected) {
 // itemsToCount: an object specifying what to count
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
-  for (const item of allItems) {
+  for (const item of Object.keys(allItems)) {
     console.log(item);
     if (itemsToCount [item]) {
       if (!results[item]) results[item] = 1;
---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) console.log(` 💯 Assertion Passed: ${actual} === ${expected}`);
+  else console.log(` 💩 Assertion Failed: ${actual} != ${expected}`);
+};
+
+const findKeyByValue = function(bestTVShow, TVShow) {
+  let values = Object.entries(bestTVShow);
+  //console.log(values);
+  for (let i = 0; i < values.length; i++) {
+    //console.log(values[i]);
+    for (let j = 0; j < values[i].length; j++) {
+      if (values[i][j] === TVShow) {
+        //console.log(values[i][j], values[i][j - 1]);
+        return values[i][j - 1];
+      }
+    }
+  }
+  return "Not found";
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
