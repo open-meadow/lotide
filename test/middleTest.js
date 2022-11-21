6bfd2cd1c6fca69d4a7e09d028226bd918e308cb
open-meadow <rohan.varughese@protonmail.com>
2022-11-21 19:48:57 +0000

    Refactor middle.js and tail.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,13 +1,43 @@
+const { expect } = require('chai');
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
 
+const assert = require("chai").assert;
 
-assertArraysEqual(middle([1]),[]); // => []
-assertArraysEqual(middle([1, 2]),[]); // => []
-assertArraysEqual(middle([1, 2, 3]),[2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]); // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+describe("# middle test", () => {
+  it("check middle value of array with one element", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it("check middle value of array with two elements", () => {
+    assert.deepEqual(middle([1, 2]),[]);
+  });
+
+  it("check middle value of array with odd number of elements", () => {
+    assert.deepEqual(middle([1, 2, 3]),[2]);
+    assert.deepEqual(middle([1, 2, 3, 4, 5]),[3]); // can you have two?
+  });
+
+  it("check middle value of array with odd number of elements", () => {
+    assert.deepEqual(middle([1, 2, 3]),[2]);
+  });
+
+  it("check middle values of array with even number of elements", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]),[2, 3]);
+  });
+
+  it("check middle values of array with even number of elements", () => {
+    assert.deepEqual(middle([143, 212, 134, 463, 115, 986]),[134, 463]);
+  });
+})
+
+
+// assertArraysEqual(middle([1]),[]); // => []
+// assertArraysEqual(middle([1, 2]),[]); // => []
+// assertArraysEqual(middle([1, 2, 3]),[2]); // => [2]
+// assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]); // => [3]
+// assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]); // => [2, 3]
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
 
 // console.log(middle([]));
 // console.log(middle([1]));
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,23 @@
 const tail = require('../tail');
 const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("# tail test", () => {
+  it("Yo yo lighthouse labs to Lighthouse Labs", () => { 
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
 
-const word = [];
-tail(word);
-assertEqual(word.length, 1);
+  it("Is blank returning blank", () => assert.deepEqual(tail([]),[]));
+
+  it("poopoo, peepee, poopy to peepee, poopy", () => { 
+    assert.deepEqual(tail(["poopoo", "peepee", "poopy"]), ["peepee", "poopy"]);
+  });
+});
+
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+
+// const word = [];
+// tail(word);
+// assertEqual(word.length, 1);
