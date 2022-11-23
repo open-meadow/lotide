cd6babd7269e9c6f08bf6f0f8c1979639a2da2be
open-meadow <rohan.varughese@protonmail.com>
2022-11-23 15:50:17 +0000

    Change eqArrays to use recursion

--- eqArrays.js
+++ eqArrays.js
@@ -10,10 +10,26 @@ const eqArrays = function(arr1, arr2) {
   if (arr1.length != arr2.length) return false;
   
   let check = 0;
-  for (let i = 0; i < arr1.length; i++) {
+  for (let i = 0; i < arr1.length; i++) { 
+
+    // console.log(arr1[i], arr2[i]);
+    
+    if(Array.isArray(arr1[i]) === true || Array.isArray(arr2[i]) === true) {
+      // console.log(eqArrays(arr1[i], arr2[i]));
+      if(eqArrays(arr1[i], arr2[i]) === true) {
+        check++;
+        // console.log(check);
+      }
+      else continue;
+    }
+
     if (arr1[i] === arr2[i]) check++; //check if value is same
   }
-  
+
+  // console.log(check);
+  // console.log(arr1.length);
+  // console.log(check === arr1.length);
+
   return check === arr1.length; //if every value is same, return true. or false.
 };
 
--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -6,3 +6,8 @@ assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
